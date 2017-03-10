c43db7641778e0b12427876db00e775ca35277bf
Diego Botelho <dibmartins@gmail.com>
2017-03-10 18:46:48 -0300

    corrigindo o nome do método post

--- src/index.php
+++ src/index.php
@@ -6,7 +6,7 @@ $api = new \Superlogica\Api('3Juh5oflLJEO', 'fRtGarWJ4aCT');
 
 $clientes = new \Superlogica\Clientes($api);
 
-$response = $clientes->adicionar([
+$response = $clientes->post([
     'ST_NOME_SAC'           => 'Cliente 5',
     'ST_NOMEREF_SAC'        => 'Nome Fantasia 5',
     'ST_DIAVENCIMENTO_SAC'  => '20',
